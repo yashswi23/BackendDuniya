a52ae7acfee17159725dabfa8f2e8a3063853e92
yashswi23 <yashswi2314@gmail.com>
2025-02-10 19:11:34 +0530

    Creating Routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const app = express();
-
-app.listen(3000);
+const port = 3000;
+app.use("/hello/2",(req,res)=>{
+    res.send("Hello from Hello/2 Route");
+})
+//IF The order of hello is above then this doesnt work the same It does not go to hello/2;
+app.use("/hello",(req,res)=>{
+    res.send("Hello from Hello Route");
+})
+app.use("/test",(req,res)=>{
+    res.send("This is Route for testing");
+})
+app.listen(port,()=>{
+    console.log(`server is Listeing on ${port} `);
+});
